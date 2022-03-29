16f98272eb899e7c3f1599eb46d8e7242a2744a1
haerphi <haerphi@gmail.com>
2022-03-29 18:28:08 +0200

    [add] prettier + openDevTools

--- src/components/color-cube.js
+++ src/components/color-cube.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-const ColorCube = (props) => {
-  console.log("stuffs", props);
-  return <div style={{ color: props.color }}>Hello Potato</div>;
+const ColorCube = props => {
+    console.log("stuffs", props);
+    return <div style={{color: props.color}}>Hello Potato</div>;
 };
 
 export default ColorCube;
